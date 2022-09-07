7d1874d7cd2fa5712710894eef5c9ee6f796f573
xauvk <xauvkubalexanderyang@live.com>
2022-09-07 11:52:09 -0400

    commit tree d09f5aa6131c9688f95741c3d28518ac539c3d14

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,6 @@
 class PlantsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :not_found_error_message
   # GET /plants
   def index
     plants = Plant.all
@@ -18,9 +19,31 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  #UPDATE /plants
+  def update
+    plant = find_plant
+    plant.update!(plant_params)
+    render json: plant, status: :accepted
+  end
+
+  #DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  def not_found_error_message
+    render json: {error: 'Plant not found'}, status: :not_found
   end
 end
